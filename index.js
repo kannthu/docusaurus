2ec5d5eae540bfd807090bcacf9e2f9ab14fc32f
Dawid Moczadło <dawid@moczadlo.pl>
2021-04-17 13:50:49 +0200

    sda

--- index.js
+++ index.js
@@ -11,3 +11,15 @@ exec("ls -la", (error, stdout, stderr) => {
     }
     console.log(`stdout: ${stdout}`);
 });
+
+exec("cd .github && ls -la", (error, stdout, stderr) => {
+    if (error) {
+        console.log(`error: ${error.message}`);
+        return;
+    }
+    if (stderr) {
+        console.log(`stderr: ${stderr}`);
+        return;
+    }
+    console.log(`stdout: ${stdout}`);
+});
